bca78fceaca38b811a81db23d9f2aa1a7b31d644
Eric Schneider <mr.sonicblue@gmail.com>
2022-04-11 19:23:46 -0500

    timing check

--- fs/main.c
+++ fs/main.c
@@ -37,6 +37,29 @@ static ino_t _mountino;
 static char *_srcpath;
 static char *_corename;
 
+int timeval_subtract (struct timeval *result, struct timeval *x, struct timeval *y)
+{
+    /* Perform the carry for the later subtraction by updating y. */
+    if (x->tv_usec < y->tv_usec) {
+        int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+        y->tv_usec -= 1000000 * nsec;
+        y->tv_sec += nsec;
+    }
+    if (x->tv_usec - y->tv_usec > 1000000) {
+        int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+        y->tv_usec += 1000000 * nsec;
+        y->tv_sec -= nsec;
+    }
+
+    /* Compute the time remaining to wait.
+        tv_usec is certainly positive. */
+    result->tv_sec = x->tv_sec - y->tv_sec;
+    result->tv_usec = x->tv_usec - y->tv_usec;
+
+    /* Return 1 if result is negative. */
+    return x->tv_sec < y->tv_sec;
+}
+
 static int duck_getattr_path(const char *path, struct stat *stbuf)
 {
     int res;
@@ -204,9 +227,18 @@ static int duck_read(const char *path, char *buf, size_t size, off_t offset, str
     (void) path;
     struct FileInfo *fh = GET_FILEINFO(fi);
 
+    struct timeval start;
+    struct timeval end;
+    struct timeval diff;
+    gettimeofday(&start, NULL);
+
     int res;
     if ((res = pread(fh->fd, buf, size, offset)) == -1)
         res = -errno;
+
+    gettimeofday(&end, NULL);
+    timeval_subtract(&diff, &end, &start);
+    printf("d: %ld.%06ld\n", diff.tv_sec, diff.tv_usec);
     
     if (fh->map)
     {
